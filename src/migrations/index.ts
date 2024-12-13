ca35d9885e5073310aba74c1a81d6b1a92c5d060
Max Karkowski <max.karkowski@aoe.com>
2024-12-13 16:51:10 +0100

    add migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241213_155054 from './20241213_155054';
+
+export const migrations = [
+  {
+    up: migration_20241213_155054.up,
+    down: migration_20241213_155054.down,
+    name: '20241213_155054'
+  },
+];
