10859ccf36ef419831b2ab0a32ad11e3ea232330
Max Karkowski <max.karkowski@aoe.com>
2024-12-20 14:40:04 +0100

    add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241216_094917 from './20241216_094917';
 import * as migration_20241216_101457 from './20241216_101457';
 import * as migration_20241220_122918 from './20241220_122918';
 import * as migration_20241220_130812 from './20241220_130812';
+import * as migration_20241220_133958 from './20241220_133958';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20241220_130812.up,
     down: migration_20241220_130812.down,
-    name: '20241220_130812'
+    name: '20241220_130812',
+  },
+  {
+    up: migration_20241220_133958.up,
+    down: migration_20241220_133958.down,
+    name: '20241220_133958'
   },
 ];
