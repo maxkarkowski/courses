0fa3b18fff15844dcb7f58653b7b32539aa6cc44
Max Karkowski <max.karkowski@aoe.com>
2024-12-20 16:46:15 +0100

    remove migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,11 +1,8 @@
-import * as migration_20241216_094917 from './20241216_094917';
-import * as migration_20241216_101457 from './20241216_101457';
-import * as migration_20241220_122918 from './20241220_122918';
-import * as migration_20241220_130812 from './20241220_130812';
-import * as migration_20241220_133958 from './20241220_133958';
-import * as migration_20241220_151850 from './20241220_151850';
-import * as migration_20241220_152517 from './20241220_152517';
-import * as migration_20241220_153608 from './20241220_153608';
+import * as migration_20241216_094917 from './20241216_094917'
+import * as migration_20241216_101457 from './20241216_101457'
+import * as migration_20241220_122918 from './20241220_122918'
+import * as migration_20241220_130812 from './20241220_130812'
+import * as migration_20241220_133958 from './20241220_133958'
 
 export const migrations = [
   {
@@ -33,19 +30,4 @@ export const migrations = [
     down: migration_20241220_133958.down,
     name: '20241220_133958',
   },
-  {
-    up: migration_20241220_151850.up,
-    down: migration_20241220_151850.down,
-    name: '20241220_151850',
-  },
-  {
-    up: migration_20241220_152517.up,
-    down: migration_20241220_152517.down,
-    name: '20241220_152517',
-  },
-  {
-    up: migration_20241220_153608.up,
-    down: migration_20241220_153608.down,
-    name: '20241220_153608'
-  },
-];
+]
