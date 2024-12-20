a3f8fbeee255e412bc050816e51f9e6a76fc15b4
Max Karkowski <max.karkowski@aoe.com>
2024-12-20 17:15:31 +0100

    deleting migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241220_160443 from './20241220_160443';
-
-export const migrations = [
-  {
-    up: migration_20241220_160443.up,
-    down: migration_20241220_160443.down,
-    name: '20241220_160443'
-  },
-];
