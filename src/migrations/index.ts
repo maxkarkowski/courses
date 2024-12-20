a038873d3f9dfca4efe07fc470ae36d2a2b99b45
Max Karkowski <max.karkowski@aoe.com>
2024-12-20 17:21:07 +0100

    add new migratuin

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241220_162056 from './20241220_162056';
+
+export const migrations = [
+  {
+    up: migration_20241220_162056.up,
+    down: migration_20241220_162056.down,
+    name: '20241220_162056'
+  },
+];
